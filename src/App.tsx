8a6b2bf93738db334d83b3dab848249c18922763
pasindulaksara <pasindulaksara@gmail.com>
2026-07-07 17:39:09 +0530

    Fix Netlify routing and scroll to top on page change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import Header from "./components/common/Header";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Footer from "./components/common/Footer";
+
 import SriLanka6N7DTour from "./pages/tours/SriLanka6N7DTour";
 import Journeys from "./pages/Journeys";
 import SriLanka8N9DTour from "./pages/tours/SriLanka8N9DTour";
@@ -12,33 +15,75 @@ import CulturalTriangleTour from "./pages/tours/CulturalTriangleTour";
 import HillCountryTour from "./pages/tours/HillCountryTour";
 import FamilyTour from "./pages/tours/FamilyTour";
 import HoneymoonTour from "./pages/tours/HoneymoonTour";
+
 import ExploreSriLanka from "./pages/ExploreSriLanka";
 import Experiences from "./pages/Experiences";
 import ExploreTheWorld from "./pages/ExploreTheWorld";
 import WorldCountryPage from "./pages/world/WorldCountryPage";
+
 import FloatingWhatsApp from "./components/common/FloatingWhatsApp";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <>
+      <ScrollToTop />
+
       <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
-        <Route path="/explore-sri-lanka/6n-7d-sri-lanka-tour" element={<SriLanka6N7DTour />}/>
+
+        <Route
+          path="/explore-sri-lanka/6n-7d-sri-lanka-tour"
+          element={<SriLanka6N7DTour />}
+        />
         <Route path="/journeys" element={<Journeys />} />
-        <Route path="/explore-sri-lanka/8n-9d-sri-lanka-tour" element={<SriLanka8N9DTour />}/>
-        <Route path="/explore-sri-lanka/cultural-triangle-tour" element={<CulturalTriangleTour />}/>
-        <Route path="/explore-sri-lanka/hill-country-tour" element={<HillCountryTour />}/>
-        <Route path="/explore-sri-lanka/family-tour" element={<FamilyTour />} />
-        <Route path="/explore-sri-lanka/honeymoon-tour" element={<HoneymoonTour />} />
+        <Route
+          path="/explore-sri-lanka/8n-9d-sri-lanka-tour"
+          element={<SriLanka8N9DTour />}
+        />
+        <Route
+          path="/explore-sri-lanka/cultural-triangle-tour"
+          element={<CulturalTriangleTour />}
+        />
+        <Route
+          path="/explore-sri-lanka/hill-country-tour"
+          element={<HillCountryTour />}
+        />
+        <Route
+          path="/explore-sri-lanka/family-tour"
+          element={<FamilyTour />}
+        />
+        <Route
+          path="/explore-sri-lanka/honeymoon-tour"
+          element={<HoneymoonTour />}
+        />
 
         <Route path="/explore-sri-lanka" element={<ExploreSriLanka />} />
         <Route path="/experiences" element={<Experiences />} />
         <Route path="/explore-the-world" element={<ExploreTheWorld />} />
-        <Route path="/explore-the-world/:countrySlug" element={<WorldCountryPage />} />
+        <Route
+          path="/explore-the-world/:countrySlug"
+          element={<WorldCountryPage />}
+        />
       </Routes>
+
       <FloatingWhatsApp />
       <Footer />
     </>
